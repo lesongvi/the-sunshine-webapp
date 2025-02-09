356d77ce890a8afa0845960a4a08af1512645d36
lesongvi <vi@lsv.vn>
2025-02-09 23:03:12 +0700

    Cập nhật hướng dẫn định nghĩa route trong web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Cái này là route mặc định của Laravel
+// Bạn define route và component tương ứng với route đó ở đây
+// Ví dụ: Khi vào trang chủ (không có đường dẫn nào tương ứng với /), thì sẽ render component Welcome
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -13,6 +16,8 @@ Route::get('/', function () {
     ]);
 });
 
+// Route này sẽ render component Login
+// Ở đây bạn có thể dùng thêm middleware để kiểm tra quyền truy cập, ví dụ ở đây tôi dùng auth:sanctum để kiểm tra xem user đã đăng nhập chưa trước khi vào trang dashboard
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
